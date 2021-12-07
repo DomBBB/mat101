633024eb92673bc2611417f7e738e8b6f8dcccda
DomBBB <48671300+DomBBB@users.noreply.github.com>
2021-12-07 22:40:02 +0100

    cleanup

--- Basic/basicTests.py
+++ Basic/basicTests.py
@@ -1,11 +1,15 @@
-from Final.Basic.basicOrganismsAndEnvironment import Organism, Environment
+from basicOrganismsAndEnvironment import Organism, Environment
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# CHANGE THESE TO LAWS THAT MAKE SENSE FOR A CERTAIN GEOMETRY --> disks, spheres, ...
-
-def save_basicTest_variableSize(file_folder=None):
+##############
+# Organism Tests
+##############
+"""
+CHANGE THESE TO LAWS THAT MAKE SENSE FOR A CERTAIN GEOMETRY --> disks, spheres, ...
+"""
+def basicTest_variableSize(file_folder=None):
     res = []
     labels = []
 
@@ -16,10 +20,11 @@ def save_basicTest_variableSize(file_folder=None):
         labels.append("organismsize_variableRate" + "__t" + str(trial+1))
         labels.append("organismsize_fixedRate" + "__t" + str(trial+1))
         items = []
-        for num in range(1001):
-            items.append([num, organism1.size, organism2.size])
+        items.append([0, organism1.size, organism2.size])
+        for num in range(1000):
             organism1.update()
             organism2.update()
+            items.append([num+1, organism1.size, organism2.size])
         res.append(items)
 
     # STORE
@@ -39,11 +44,11 @@ def save_basicTest_variableSize(file_folder=None):
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
     for column in df.columns[1:3]:
-        df[column].plot(ax=axes[0], x=df.columns[0], label=column)
+        df[column].plot(ax=axes[0], x=df.columns[0])
     for column in df.columns[4:6]:
-        df[column].plot(ax=axes[1], x=df.columns[3], label=column)
+        df[column].plot(ax=axes[1], x=df.columns[3])
     for column in df.columns[7:9]:
-        df[column].plot(ax=axes[2], x=df.columns[6], label=column)
+        df[column].plot(ax=axes[2], x=df.columns[6], label=column[:column.find("__t")])
 
     axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
           fancybox=True, shadow=True, ncol=5)
@@ -53,21 +58,24 @@ def save_basicTest_variableSize(file_folder=None):
     # df.to_csv(file_folder + "_basicTest_variableSize.csv")
     # df = pd.read_csv(file_folder + "_basicTest_variableSize.csv", index_col=0)
 
-# CHANGE THESE TO TEST DIFFERENT THINGS
 
-def save_basicTest_Environment1(file_folder=None):
+#################
+# Environment Tests
+#################
+def basicTest_Environment1(file_folder=None):
     res = []
     labels = []
 
     for trial in range(3):
-        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
         environment1 = Environment(10**3, 100, [organism1])
         labels.append("cycle" + "__t" + str(trial+1))
         labels.append("population" + "__t" + str(trial+1))
         items = []
-        for num in range(1001):
-            items.append([num, len(environment1.population)])
+        items.append([0, len(environment1.population)])
+        for num in range(100):
             environment1.update()
+            items.append([num+1, len(environment1.population)])
         res.append(items)
 
     # STORE
@@ -83,11 +91,11 @@ def save_basicTest_Environment1(file_folder=None):
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
     for column in df.columns[1:2]:
-        df[column].plot(ax=axes[0], x=df.columns[0], label=column)
+        df[column].plot(ax=axes[0], x=df.columns[0])
     for column in df.columns[3:4]:
-        df[column].plot(ax=axes[1], x=df.columns[2], label=column)
+        df[column].plot(ax=axes[1], x=df.columns[2])
     for column in df.columns[5:6]:
-        df[column].plot(ax=axes[2], x=df.columns[4], label=column)
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
 
     axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
           fancybox=True, shadow=True, ncol=5)
@@ -97,113 +105,263 @@ def save_basicTest_Environment1(file_folder=None):
     #df.to_csv(file_folder + "_basicTest_Environment1.csv")
     # df = pd.read_csv(file_folder + "_basicTest_Environment1.csv", index_col=0)
 
+def basicTest_Environment2(file_folder=None):
+    res = []
+    labels = []
 
+    for trial in range(3):
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
+        environment1 = Environment(10**3, 100, [organism1])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("population" + "__t" + str(trial+1))
+        items = []
+        items.append([0, len(environment1.population)])
+        for num in range(5*10**3):
+            environment1.update()
+            items.append([num+1, len(environment1.population)])
+        res.append(items)
 
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[1][idx][0],
+                                               labels[3]: res[1][idx][1],
+                                               labels[4]: res[2][idx][0],
+                                               labels[5]: res[2][idx][1]})
 
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
+    for column in df.columns[1:2]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[3:4]:
+        df[column].plot(ax=axes[1], x=df.columns[2])
+    for column in df.columns[5:6]:
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
 
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
 
-organism1 = Organism(100, 200, lambda x : 1/4 * x**(2/3), lambda x : 1/50 * x, "SizeGuys")
-organism2 = Organism(100, 200, lambda x : 5, lambda x : 2, "SpecimentZero")
+    df
+    #df.to_csv(file_folder + "_basicTest_Environment2.csv")
+    # df = pd.read_csv(file_folder + "_basicTest_Environment2.csv", index_col=0)
 
-environment1 = Environment(10**3, 100, [organism1])
+def basicTest_Environment3(file_folder=None):
+    res = []
+    labels = []
 
+    for trial in range(3):
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
+        environment1 = Environment(10**3, 100, [organism1])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("divisions" + "__t" + str(trial+1))
+        items = []
+        items.append([0, len(environment1.population)])
+        for num in range(5*10**3):
+            items.append([num+1, environment1.update()])
+        res.append(items)
 
-cap = 100
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[1][idx][0],
+                                               labels[3]: res[1][idx][1],
+                                               labels[4]: res[2][idx][0],
+                                               labels[5]: res[2][idx][1]})
 
-for round in range(0, cap):
-    print(f"Round {round + 1}: ", end="")
-    environment1.update()
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
+    for column in df.columns[1:2]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[3:4]:
+        df[column].plot(ax=axes[1], x=df.columns[2])
+    for column in df.columns[5:6]:
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
 
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
 
+    df
+    #df.to_csv(file_folder + "_basicTest_Environment3.csv")
+    # df = pd.read_csv(file_folder + "_basicTest_Environment3.csv", index_col=0)
 
+def basicTest_Environment4(file_folder=None):
+    res = []
+    labels = []
 
-organism2 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
-environment2 = Environment(10**3, 100, [organism2])
-res2 = []
-for num in range(5*10**3):
-    environment2.update()
-    res2.append((num+1, len(environment2.population)))
-res2
-idx_ls = []
-pop_ls = []
-for item in res2:
-    idx_ls.append(item[0])
-    pop_ls.append(item[1])
-plt.plot(idx_ls, pop_ls)
-plt.show()
+    for trial in range(3):
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
+        environment1 = Environment(10**3, 10**3, [organism1])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("population" + "__t" + str(trial+1))
+        items = []
+        items.append([0, len(environment1.population)])
+        for num in range(5*10**3):
+            environment1.update()
+            items.append([num+1, len(environment1.population)])
+        res.append(items)
 
-organism3 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
-environment3 = Environment(10**3, 100, [organism3])
-res3 = []
-for num in range(5*10**3):
-    res3.append((num+1, environment3.update()))
-res3
-idx_ls = []
-div_ls = []
-for item in res3:
-    idx_ls.append(item[0])
-    div_ls.append(item[1])
-plt.plot(idx_ls, div_ls)
-plt.show()
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[1][idx][0],
+                                               labels[3]: res[1][idx][1],
+                                               labels[4]: res[2][idx][0],
+                                               labels[5]: res[2][idx][1]})
 
-organism4 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
-environment4= Environment(10**3, 10**3, [organism4])
-res4 = []
-for num in range(5*10**3):
-    environment4.update()
-    res4.append((num+1, len(environment4.population)))
-res4
-idx_ls = []
-pop_ls = []
-for item in res4:
-    idx_ls.append(item[0])
-    pop_ls.append(item[1])
-plt.plot(idx_ls, pop_ls)
-plt.show()
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
-organism5 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
-environment5 = Environment(10**3, 10**3, [organism5])
-res5 = []
-for num in range(5*10**3):
-    res5.append((num+1, environment5.update()))
-res5
-idx_ls = []
-div_ls = []
-for item in res5:
-    idx_ls.append(item[0])
-    div_ls.append(item[1])
-plt.plot(idx_ls, div_ls)
-plt.show()
+    for column in df.columns[1:2]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[3:4]:
+        df[column].plot(ax=axes[1], x=df.columns[2])
+    for column in df.columns[5:6]:
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
 
-#ENV(0, 100) oder (0, 1000)
-organism6 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
-environment6= Environment(0, 100, [organism6])
-res6 = []
-for num in range(5*10**3):
-    environment6.update()
-    res6.append((num+1, len(environment6.population)))
-res6
-idx_ls = []
-pop_ls = []
-for item in res6:
-    idx_ls.append(item[0])
-    pop_ls.append(item[1])
-plt.plot(idx_ls, pop_ls)
-plt.show()
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
 
-#ENV(0, 100) oder (0, 1000)
-organism7 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen Zero")
-environment7 = Environment(0, 100, [organism7])
-res7 = []
-for num in range(5*10**3):
-    res7.append((num+1, environment7.update()))
-res7
-idx_ls = []
-div_ls = []
-for item in res7:
-    idx_ls.append(item[0])
-    div_ls.append(item[1])
-plt.plot(idx_ls, div_ls)
-plt.show()
+    df
+    #df.to_csv(file_folder + "_basicTest_Environment4.csv")
+    # df = pd.read_csv(file_folder + "_basicTest_Environment4.csv", index_col=0)
+
+def basicTest_Environment5(file_folder=None):
+    res = []
+    labels = []
+
+    for trial in range(3):
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
+        environment1 = Environment(10**3, 10**3, [organism1])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("divisions" + "__t" + str(trial+1))
+        items = []
+        items.append([0, len(environment1.population)])
+        for num in range(5*10**3):
+            items.append([num+1, environment1.update()])
+        res.append(items)
+
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[1][idx][0],
+                                               labels[3]: res[1][idx][1],
+                                               labels[4]: res[2][idx][0],
+                                               labels[5]: res[2][idx][1]})
+
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
+
+    for column in df.columns[1:2]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[3:4]:
+        df[column].plot(ax=axes[1], x=df.columns[2])
+    for column in df.columns[5:6]:
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
+
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
+
+    df
+    #df.to_csv(file_folder + "_basicTest_Environment5.csv")
+    # df = pd.read_csv(file_folder + "_basicTest_Environment5.csv", index_col=0)
+
+"""
+Or refill = 10**3 and food = 0 (p. 9-10 from artificial_life.pdf)
+"""
+def basicTest_Environment6(file_folder=None):
+    res = []
+    labels = []
+
+    for trial in range(3):
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
+        environment1 = Environment(0, 100, [organism1])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("population" + "__t" + str(trial+1))
+        items = []
+        items.append([0, len(environment1.population)])
+        for num in range(5*10**3):
+            environment1.update()
+            items.append([num+1, len(environment1.population)])
+        res.append(items)
+
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[1][idx][0],
+                                               labels[3]: res[1][idx][1],
+                                               labels[4]: res[2][idx][0],
+                                               labels[5]: res[2][idx][1]})
+
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
+
+    for column in df.columns[1:2]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[3:4]:
+        df[column].plot(ax=axes[1], x=df.columns[2])
+    for column in df.columns[5:6]:
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
+
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
+
+    df
+    #df.to_csv(file_folder + "_basicTest_Environment6.csv")
+    # df = pd.read_csv(file_folder + "_basicTest_Environment6.csv", index_col=0)
+
+"""
+Or refill = 10**3 and food = 0 (p. 9-10 from artificial_life.pdf)
+"""
+def basicTest_Environment7(file_folder=None):
+    res = []
+    labels = []
+
+    for trial in range(3):
+        organism1 = Organism(100, 200, lambda x : 5, lambda x : 2, "Specimen One")
+        environment1 = Environment(0, 100, [organism1])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("divisions" + "__t" + str(trial+1))
+        items = []
+        items.append([0, len(environment1.population)])
+        for num in range(5*10**3):
+            items.append([num+1, environment1.update()])
+        res.append(items)
+
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[1][idx][0],
+                                               labels[3]: res[1][idx][1],
+                                               labels[4]: res[2][idx][0],
+                                               labels[5]: res[2][idx][1]})
+
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
+
+    for column in df.columns[1:2]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[3:4]:
+        df[column].plot(ax=axes[1], x=df.columns[2])
+    for column in df.columns[5:6]:
+        df[column].plot(ax=axes[2], x=df.columns[4], label=column[:column.find("__t")])
+
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
+
+    df
+    #df.to_csv(file_folder + "_basicTest_Environment7.csv")
+    # df = pd.read_csv(file_folder + "_basicTest_Environment7.csv", index_col=0)
--- Basic/naturalSelectionTests.py
+++ Basic/naturalSelectionTests.py
@@ -1,35 +1,65 @@
-from Basic.basicOrganisms import Organism, Environment
+from basicOrganismsAndEnvironment import Organism, Environment
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# CHANGE THESE (RATES) TO MAKE OTHER TESTS
-organism_active = Organism(100, 200, lambda x : 5, lambda x : 2, "active")
-organism_passive = Organism(100, 200, lambda x : 4, lambda x : 1, "passive")
 
-# CHANGE THESE TO MAKE OTHER TESTS
-environment = Environment(10**3, 1000, [organism_active, organism_passive])
+def naturalSelectionTest(file_folder=None):
+    res = []
+    labels = []
 
-res = []
-for num in range(2*10**3):
-    environment.update()
-    active = 0
-    passive = 0
-    for organism in environment.population:
-        if organism.species == "active":
-            active += 1
-        else:
-            passive += 1
-    res.append((num+1, len(environment.population), active, passive))
-res
-idx_ls = []
-pop_ls = []
-act_ls = []
-pas_ls = []
-for item in res:
-    idx_ls.append(item[0])
-    pop_ls.append(item[1])
-    act_ls.append(item[2])
-    pas_ls.append(item[3])
-plt.plot(idx_ls, pop_ls)
-plt.plot(idx_ls, act_ls)
-plt.plot(idx_ls, pas_ls)
-plt.show()
+    for trial in range(3):
+        organism_active = Organism(100, 200, lambda x : 5, lambda x : 2, "active")
+        organism_passive = Organism(100, 200, lambda x : 4, lambda x : 1, "passive")
+        environment1 = Environment(10**3, 10**3, [organism_active, organism_passive])
+        labels.append("cycle" + "__t" + str(trial+1))
+        labels.append("population_active" + "__t" + str(trial+1))
+        labels.append("population_passive" + "__t" + str(trial+1))
+        labels.append("population_total" + "__t" + str(trial+1))
+        items = []
+        items.append([0, 1, 1, 2])
+
+        for num in range(2*10**3):
+            environment1.update()
+            active = 0
+            passive = 0
+            for organism in environment1.population:
+                if organism.species == "active":
+                    active += 1
+                elif organism.species == "passive":
+                    passive += 1
+            assert((active+passive) == len(environment1.population))
+            items.append([num+1, active, passive, active+passive])
+        res.append(items)
+
+    # STORE
+    df = pd.DataFrame(columns=labels, index=range(len(res[0])))
+    for idx, item in enumerate(res[0]):
+        df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
+                                               labels[1]: res[0][idx][1],
+                                               labels[2]: res[0][idx][2],
+                                               labels[3]: res[0][idx][3],
+                                               labels[4]: res[1][idx][0],
+                                               labels[5]: res[1][idx][1],
+                                               labels[6]: res[1][idx][2],
+                                               labels[7]: res[1][idx][3],
+                                               labels[8]: res[2][idx][0],
+                                               labels[9]: res[2][idx][1],
+                                               labels[10]: res[2][idx][2],
+                                               labels[11]: res[2][idx][3]})
+
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
+
+    for column in df.columns[1:4]:
+        df[column].plot(ax=axes[0], x=df.columns[0])
+    for column in df.columns[5:8]:
+        df[column].plot(ax=axes[1], x=df.columns[4])
+    for column in df.columns[9:12]:
+        df[column].plot(ax=axes[2], x=df.columns[8], label=column[:column.find("__t")])
+
+    axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+          fancybox=True, shadow=True, ncol=5)
+    plt.show
+
+    df
+    #df.to_csv(file_folder + "naturalSelectionTest.csv")
+    # df = pd.read_csv(file_folder + "naturalSelectionTest.csv", index_col=0)
