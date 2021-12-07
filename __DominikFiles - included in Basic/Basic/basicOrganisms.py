5b605b7dc6e22caf964a97b5aaca49c30e781ace
DomBBB <48671300+DomBBB@users.noreply.github.com>
2021-12-07 21:13:59 +0100

    cleanup

---
+++ __DominikFiles - included in Basic/Basic/basicOrganisms.py
@@ -0,0 +1,78 @@
+import random
+
+class Organism():
+    def __init__(self, size, division_threshold, uptake_rate, metabolic_rate, species="unknown"):
+        self.size = float(size)
+        self.division_threshold = float(division_threshold)
+        self.uptake_rate = uptake_rate
+        self.metabolic_rate = metabolic_rate
+        self.alive = True
+        self.species = species
+
+    def update(self, available_food=None):
+        if self.alive:
+            if available_food == None:
+                uptake = self.uptake_rate(self.size)
+            else:
+                uptake = min(available_food, self.uptake_rate(self.size))
+            self.size = self.size + uptake - self.metabolic_rate(self.size)
+            if self.size <= 0:
+                self.alive = False
+                return "Dead", float(uptake)
+            if self.size > self.division_threshold:
+                return True, float(uptake)
+            else:
+                return False, float(uptake)
+        else:
+            return "Dead", 0
+
+
+
+class Environment():
+    def __init__(self, food, refill_rate, population):
+        self.food = float(food)
+        self.refill_rate = float(refill_rate)
+        self.population = population
+
+    def update(self):
+        # Randomly shuffles a copy of the population list
+            # AFTER shuffling we don't know the index in the original population:
+            # --> WHY do we copy? Shouldn't we replace the population with the new population?
+            # --> --> Do we remove dead ones?
+        shuffle_list = self.population.copy()
+        random.shuffle(shuffle_list)
+
+        # Counts the total number of divisions that occur in an update cycle, and returns that value.
+        no_of_divisions = 0
+        new_organism_list = []
+
+        # For each organism in the shuffled list
+        for organism in shuffle_list:
+            # Update the organism
+            divide, eaten_food = organism.update(self.food)
+            # Reduce food by the amount the organism ate.
+            self.food = self.food - eaten_food
+
+            # Not in task
+                # If we want to remove dead organisms
+            if divide == "Dead":
+                continue
+            # If the organism returns the division signal True: Remove organism from population & add 2 new (same parameters, size/2)
+            elif divide:
+                size = organism.__dict__["size"]
+                division_threshold = organism.__dict__["division_threshold"]
+                uptake_rate = organism.__dict__["uptake_rate"]
+                metabolic_rate = organism.__dict__["metabolic_rate"]
+                species = organism.__dict__["species"]
+                new_organism_list.append(Organism(size/2, division_threshold, uptake_rate, metabolic_rate, species))
+                new_organism_list.append(Organism(size/2, division_threshold, uptake_rate, metabolic_rate, species))
+                no_of_divisions = no_of_divisions + 1
+            else:
+                new_organism_list.append(organism)
+
+        self.population = new_organism_list
+        # After all organisms have been updated, increase food by the refill_rate.
+        self.food = self.food + self.refill_rate
+
+        # Counts the total number of divisions that occur in an update cycle, and returns that value.
+        return no_of_divisions
