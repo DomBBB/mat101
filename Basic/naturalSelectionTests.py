abba3850817b9ae2f2375fae2e92b3cd3913a8f3
DomBBB <48671300+DomBBB@users.noreply.github.com>
2021-12-10 14:46:08 +0100

    comment naturalSelection.py

--- Basic/naturalSelectionTests.py
+++ Basic/naturalSelectionTests.py
@@ -4,9 +4,16 @@ import pandas as pd
 
 
 def save_naturalSelectionTest1(file_folder):
+    """
+    This function stores the results from this test that is done in triplicate
+    in a csv file.
+    """
     res = []
     labels = ["cycle"]
 
+    # The simulation is done three times to account for randomness in the
+    # results. Corresponding labels are created and the results are
+    # appended to a list.
     for trial in range(3):
         organism_active = Organism(100, 200, lambda x : 5, lambda x : 2, "active")
         organism_passive = Organism(100, 200, lambda x : 4, lambda x : 1, "passive")
@@ -16,7 +23,6 @@ def save_naturalSelectionTest1(file_folder):
         labels.append("population_total" + "__t" + str(trial+1))
         items = []
         items.append([0, 1, 1, 2])
-
         for num in range(2*10**3):
             environment1.update()
             active = 0
@@ -30,7 +36,8 @@ def save_naturalSelectionTest1(file_folder):
             items.append([num+1, active, passive, active+passive])
         res.append(items)
 
-    # STORE
+    # An empty dataframe with the labels and an appropriate number of entries
+    # is created and then filled with the corresponding values.
     df = pd.DataFrame(columns=labels, index=range(len(res[0])))
     for idx, item in enumerate(res[0]):
         df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
@@ -44,15 +51,23 @@ def save_naturalSelectionTest1(file_folder):
                                                labels[8]: res[2][idx][2],
                                                labels[9]: res[2][idx][3]})
 
+    # The dataframe is stored as a csv file.
     df.to_csv(file_folder + "naturalSelectionTest1.csv")
 
 def retrieve_naturalSelectionTest1(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots all trials.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "naturalSelectionTest1.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with subplots is created and formating attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
+    # The subplots are filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes[0], x=df.columns[0])
     for column in df.columns[4:7]:
@@ -60,6 +75,7 @@ def retrieve_naturalSelectionTest1(file_folder):
     for column in df.columns[7:10]:
         df[column].plot(ax=axes[2], x=df.columns[0])
 
+    # The plot is labeled properly.
     axes[0].set_title("Natural Selection", fontsize = 18)
     axes[2].set_xlabel("Time", fontsize=14)
     axes[2].legend(["Active: 5 & 2", "Passive: 4 & 1", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.6),
@@ -68,15 +84,23 @@ def retrieve_naturalSelectionTest1(file_folder):
     plt.show
 
 def retrieve_naturalSelectionTest1__t1(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots only one trial.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "naturalSelectionTest1.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with one subplot is created and formatting attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
 
+    # The subplot is filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes, x=df.columns[0])
 
+    # The plot is labeled properly.
     axes.set_title("Natural Selection - Trial 1", fontsize = 18)
     axes.set_xlabel("Time", fontsize=14)
     axes.legend(["Active: 5 & 2", "Passive: 4 & 1", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.16),
@@ -90,9 +114,16 @@ retrieve_naturalSelectionTest1__t1("DataCollection/")
 
 
 def save_naturalSelectionTest2(file_folder):
+    """
+    This function stores the results from this test that is done in triplicate
+    in a csv file.
+    """
     res = []
     labels = ["cycle"]
 
+    # The simulation is done three times to account for randomness in the
+    # results. Corresponding labels are created and the results are
+    # appended to a list.
     for trial in range(3):
         organism_active = Organism(100, 200, lambda x : 5, lambda x : 2, "active")
         organism_passive = Organism(100, 200, lambda x : 4.9, lambda x : 1.9, "passive")
@@ -102,7 +133,6 @@ def save_naturalSelectionTest2(file_folder):
         labels.append("population_total" + "__t" + str(trial+1))
         items = []
         items.append([0, 1, 1, 2])
-
         for num in range(2*10**3):
             environment1.update()
             active = 0
@@ -116,7 +146,8 @@ def save_naturalSelectionTest2(file_folder):
             items.append([num+1, active, passive, active+passive])
         res.append(items)
 
-    # STORE
+    # An empty dataframe with the labels and an appropriate number of entries
+    # is created and then filled with the corresponding values.
     df = pd.DataFrame(columns=labels, index=range(len(res[0])))
     for idx, item in enumerate(res[0]):
         df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
@@ -130,15 +161,23 @@ def save_naturalSelectionTest2(file_folder):
                                                labels[8]: res[2][idx][2],
                                                labels[9]: res[2][idx][3]})
 
+    # The dataframe is stored as a csv file.
     df.to_csv(file_folder + "naturalSelectionTest2.csv")
 
 def retrieve_naturalSelectionTest2(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots all trials.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "naturalSelectionTest2.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with subplots is created and formating attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
+    # The subplots are filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes[0], x=df.columns[0])
     for column in df.columns[4:7]:
@@ -146,6 +185,7 @@ def retrieve_naturalSelectionTest2(file_folder):
     for column in df.columns[7:10]:
         df[column].plot(ax=axes[2], x=df.columns[0])
 
+    # The plot is labeled properly.
     axes[0].set_title("Natural Selection", fontsize = 18)
     axes[2].set_xlabel("Time", fontsize=14)
     axes[2].legend(["Active: 5 & 2", "Passive: 4.9 & 1.9", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.6),
@@ -154,15 +194,23 @@ def retrieve_naturalSelectionTest2(file_folder):
     plt.show
 
 def retrieve_naturalSelectionTest2__t1(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots only one trial.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "naturalSelectionTest2.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with one subplot is created and formatting attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
 
+    # The subplot is filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes, x=df.columns[0])
 
+    # The plot is labeled properly.
     axes.set_title("Natural Selection - Trial 1", fontsize = 18)
     axes.set_xlabel("Time", fontsize=14)
     axes.legend(["Active: 5 & 2", "Passive: 4.9 & 1.9", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.16),
