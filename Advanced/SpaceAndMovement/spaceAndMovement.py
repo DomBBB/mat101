f8e7fc7de008ef8598e096352618ff41ed86e23b
DomBBB <48671300+DomBBB@users.noreply.github.com>
2021-12-10 16:19:06 +0100

    comment spaceAndMovement

--- Advanced/SpaceAndMovement/spaceAndMovement.py
+++ Advanced/SpaceAndMovement/spaceAndMovement.py
@@ -3,7 +3,14 @@ import math
 
 
 class Organism:
+    """
+    This class extends the basic version of an organism and adds a position to
+    the organism.
+    """
     def __init__(self, size, division_threshold, uptake_rate, metabolic_rate, species="unknown"):
+        """
+        This method initializes an organism with certain instance attributes.
+        """
         self.size = float(size)
         self.division_threshold = float(division_threshold)
         self.uptake_rate = uptake_rate
@@ -13,12 +20,27 @@ class Organism:
         self.alive = True
 
     def update(self, available_food=None):
+        """
+        This method lets the organism eat food and burn already eaten food. It
+        then returns a state describing if the organism is dead or alive and if
+        it wants to divide as well as the amount of food it ate.
+        """
         if self.alive:
+            # If update() is called without an argument this is considered as
+            # an infinite amount of food and the organism eats as much as it
+            # can take up.
             if available_food == None:
                 uptake = self.uptake_rate(self.size)
+            # Otherwise it takes up whichever number from the passed food
+            # amount and its own uptake is smaller.
             else:
                 uptake = min(available_food, self.uptake_rate(self.size))
+            # The size increases by the amount of food the organism can take up
+            # and decreases by the amount of already eaten food that the
+            # organism burns.
             self.size = self.size + uptake - self.metabolic_rate(self.size)
+            # Depending on the size of the organism different values are
+            # returned to indicate its state and the amount of food it ate.
             if self.size <= 0:
                 self.alive = False
                 return "Dead", float(uptake)
@@ -31,7 +53,16 @@ class Organism:
 
 
 class Environment():
+    """
+    This class extends the basic version of an environment and adds randomly
+    positioned food crates from which the organisms can eat when they are close
+    enough.
+    """
     def __init__(self, food, refill_rate, population, num_food_crates):
+        """
+        This method initializes an environment with certain instance
+        attributes.
+        """
         self.food = float(food)
         self.refill_rate = float(refill_rate)
         self.population = population
@@ -41,36 +72,51 @@ class Environment():
             self.food_crates.append([random.randint(0, 50), random.randint(0, 50), food/num_food_crates])
 
     def update(self):
+        """
+        This method updates all organisms in an environment by calculating the
+        distance to each food crate. If the organism is close enough food is
+        passed as an argument and then those return values are used to change
+        the environment attributes. Specifically it reduces the amount of food
+        by the amount the organism ate, decides whether the organism should
+        stay in the population, be deleted from it or be deleted from it while
+        adding two daughter organisms. If the organism is not close enough it
+        moves a random distance towards the crate. If a crate is emptied out it
+        respawns at a random position. In the end the food crates in the
+        environment are refilled according to the environment's attributes and
+        the number of divisions that occured in this update is returned.
+        """
         random.shuffle(self.population)
 
-        # Counts the total number of divisions that occur in an update cycle, and returns that value.
         no_of_divisions = 0
         new_organism_list = []
 
-        # For each organism in the shuffled list
         for organism in self.population:
-
+            # The first crate is set as closest crate and the distance from the
+            # organism to this crate is calculated (pythagoras).
             closest_crate = self.food_crates[0]
             dist_to_closest = math.sqrt((closest_crate[0] - organism.organism_position[0])**2 + (closest_crate[1] - organism.organism_position[1])**2)
-            food_in_closest = closest_crate[2]
 
-            for crate in self.food_crates:
+            # Then for each other crate the distance is also calculated
+            # (pythagoras). If it is lower than the distance to the closest
+            # this crate is temporarily set as closest.
+            for crate in self.food_crates[1:]:
                 x_crate = crate[0]
                 y_crate = crate[1]
-
                 dist = math.sqrt((x_crate - organism.organism_position[0])**2 + (y_crate - organism.organism_position[1])**2)
                 if dist < dist_to_closest:
                     closest_crate = crate
                     dist_to_closest = dist
 
-            directionx = closest_crate[0]-organism.organism_position[0]
-            directiony = closest_crate[1]-organism.organism_position[1]
-
+            # If the organism is close enough to the create the amount of food
+            # in this crate is stored.
             if dist_to_closest <= 0.1:
                 food_amount = closest_crate[2]
+            # Otherwise the organism moves a random distance towards the
+            # crate and 0 is stored as amount of food.
             else:
+                directionx = closest_crate[0]-organism.organism_position[0]
+                directiony = closest_crate[1]-organism.organism_position[1]
                 food_amount = 0
-
                 xval = organism.organism_position[0]
                 xval = xval + random.randint(min(0, directionx), max(0, directionx))
                 yval = organism.organism_position[1]
@@ -82,16 +128,18 @@ class Environment():
 
             # Reduce food in food crate by the amount the organism ate.
             closest_crate[2] = closest_crate[2] - eaten_food
+            # Respawn a crate at a random position if it is empty.
             if closest_crate[2] <= 0:
                 self.food_crates.remove(closest_crate)
                 self.food_crates.append([random.randint(0, 50),  random.randint(0, 50), 0])
-            else:
-                closest_crate[2] = closest_crate[2] - eaten_food
 
-            # We remove dead organisms
+            # Dead organisms are skipped and therefore removed from the
+            # population.
             if divide == "Dead":
                 continue
-            # If the organism returns the division signal True: Remove organism from population & add 2 new (same parameters, size/2)
+            # If the organism returns the division signal True two daughter
+            # organisms are created. They have the same attributes as the
+            # parent organism but only half the size and a random position.
             elif divide:
                 size = organism.__dict__["size"]
                 division_threshold = organism.__dict__["division_threshold"]
@@ -104,11 +152,14 @@ class Environment():
             else:
                 new_organism_list.append(organism)
 
+        # After all organisms are updated the population is cleaned so that
+        # only living organisms are contained in it.
         self.population = new_organism_list
 
-        # After all organisms have been updated, increase food by the refill_rate.
+        # Then the food in all crates is increased by an equal fraction of the
+        # refill_rate.
         for crate in self.food_crates:
             crate[2] = crate[2] + self.refill_rate/self.num_food_crates
 
-        # Counts the total number of divisions that occur in an update cycle, and returns that value.
+        # The number of divisions per update is tracked and returned here.
         return no_of_divisions
--- Advanced/SpaceAndMovement/spaceAndMovementTests.py
+++ Advanced/SpaceAndMovement/spaceAndMovementTests.py
@@ -6,9 +6,16 @@ import json
 
 
 def save_spaceAndMovementTest1(file_folder):
+    """
+    This function stores the results from this test that is done in triplicate
+    in a csv file.
+    """
     res = []
     labels = ["cycle"]
 
+    # The simulation is done three times to account for randomness in the
+    # results. Corresponding labels are created and the results are
+    # appended to a list.
     for trial in range(3):
         organism_active = Organism(100, 200, lambda x : 3, lambda x : 1, "active")
         organism_passive = Organism(100, 200, lambda x : 3, lambda x : 1, "passive")
@@ -20,7 +27,6 @@ def save_spaceAndMovementTest1(file_folder):
         labels.append("organism_positions" + "__t" + str(trial+1))
         items = []
         items.append([0, 1, 1, 2, copy.deepcopy(environment1.food_crates), [[organism_active.organism_position[0], organism_active.organism_position[1], organism_active.species], [organism_passive.organism_position[0], organism_passive.organism_position[1], organism_passive.species]]])
-
         for num in range(2*10**3):
             environment1.update()
             active = 0
@@ -36,7 +42,8 @@ def save_spaceAndMovementTest1(file_folder):
             items.append([num+1, active, passive, active+passive, copy.deepcopy(environment1.food_crates), positions])
         res.append(items)
 
-    # STORE
+    # An empty dataframe with the labels and an appropriate number of entries
+    # is created and then filled with the corresponding values.
     df = pd.DataFrame(columns=labels, index=range(len(res[0])))
     for idx, item in enumerate(res[0]):
         df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
@@ -56,15 +63,23 @@ def save_spaceAndMovementTest1(file_folder):
                                                labels[14]: res[2][idx][4],
                                                labels[15]: res[2][idx][5]})
 
+    # The dataframe is stored as a csv file.
     df.to_csv(file_folder + "spaceAndMovementTest1.csv")
 
 def retrieve_spaceAndMovementTest1(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots all trials.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "spaceAndMovementTest1.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with subplots is created and formating attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
+    # The subplots are filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes[0], x=df.columns[0])
     for column in df.columns[6:9]:
@@ -72,6 +87,7 @@ def retrieve_spaceAndMovementTest1(file_folder):
     for column in df.columns[11:14]:
         df[column].plot(ax=axes[2], x=df.columns[0])
 
+    # The plot is labeled properly.
     axes[0].set_title("Space And Movement", fontsize = 18)
     axes[2].set_xlabel("Time", fontsize=14)
     axes[2].legend(["Active: 3 & 1", "Passive: 3 & 1", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.6),
@@ -80,15 +96,23 @@ def retrieve_spaceAndMovementTest1(file_folder):
     plt.show
 
 def retrieve_spaceAndMovementTest1__t1(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots only one trial.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "spaceAndMovementTest1.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with one subplot is created and formatting attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
 
+    # The subplot is filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes, x=df.columns[0])
 
+    # The plot is labeled properly.
     axes.set_title("Space And Movement - Trial 1", fontsize = 18)
     axes.set_xlabel("Time", fontsize=14)
     axes.legend(["Active: 3 & 1", "Passive: 3 & 1", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.16),
@@ -97,7 +121,18 @@ def retrieve_spaceAndMovementTest1__t1(file_folder):
     plt.show
 
 def draw_spaceAndMovementTest1(file_folder, lower, upper, cycle):
+    """
+    This function retrieves the results from this test from a csv file and
+    draws a map of one trial (cycle argument) for the time-steps between
+    lower and upper (arguments)
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "spaceAndMovementTest1.csv", index_col=0)
+
+    # Loops through lower-upper to draw only
+    # those time-steps. In each iteration an entry is selected and the food
+    # crates are drawn in green, active organisms in red and passive
+    # organisms in blue.
     for i in range(lower, upper):
         lst_crate = json.loads(df["food_crates__"+cycle].iloc[i])
         crate_x = [x for x, y, z in lst_crate]
@@ -121,9 +156,16 @@ draw_spaceAndMovementTest1("DataCollection/", 0, 100, "t1")
 
 
 def save_spaceAndMovementTest2(file_folder):
+    """
+    This function stores the results from this test that is done in triplicate
+    in a csv file.
+    """
     res = []
     labels = ["cycle"]
 
+    # The simulation is done three times to account for randomness in the
+    # results. Corresponding labels are created and the results are
+    # appended to a list.
     for trial in range(3):
         organism_active = Organism(100, 200, lambda x : 5, lambda x : 3, "active")
         organism_passive = Organism(100, 200, lambda x : 4, lambda x : 1, "passive")
@@ -135,7 +177,6 @@ def save_spaceAndMovementTest2(file_folder):
         labels.append("organism_positions" + "__t" + str(trial+1))
         items = []
         items.append([0, 1, 1, 2, copy.deepcopy(environment1.food_crates), [[organism_active.organism_position[0], organism_active.organism_position[1], organism_active.species], [organism_passive.organism_position[0], organism_passive.organism_position[1], organism_passive.species]]])
-
         for num in range(2*10**3):
             environment1.update()
             active = 0
@@ -151,7 +192,8 @@ def save_spaceAndMovementTest2(file_folder):
             items.append([num+1, active, passive, active+passive, copy.deepcopy(environment1.food_crates), positions])
         res.append(items)
 
-    # STORE
+    # An empty dataframe with the labels and an appropriate number of entries
+    # is created and then filled with the corresponding values.
     df = pd.DataFrame(columns=labels, index=range(len(res[0])))
     for idx, item in enumerate(res[0]):
         df.loc[idx] = pd.Series({labels[0]: res[0][idx][0],
@@ -171,15 +213,23 @@ def save_spaceAndMovementTest2(file_folder):
                                                labels[14]: res[2][idx][4],
                                                labels[15]: res[2][idx][5]})
 
+    # The dataframe is stored as a csv file.
     df.to_csv(file_folder + "spaceAndMovementTest2.csv")
 
 def retrieve_spaceAndMovementTest2(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots all trials.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "spaceAndMovementTest2.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with subplots is created and formating attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
 
+    # The subplots are filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes[0], x=df.columns[0])
     for column in df.columns[6:9]:
@@ -187,6 +237,7 @@ def retrieve_spaceAndMovementTest2(file_folder):
     for column in df.columns[11:14]:
         df[column].plot(ax=axes[2], x=df.columns[0])
 
+    # The plot is labeled properly.
     axes[0].set_title("Space And Movement", fontsize = 18)
     axes[2].set_xlabel("Time", fontsize=14)
     axes[2].legend(["Active: 5 & 3", "Passive: 4 & 1", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.6),
@@ -195,15 +246,23 @@ def retrieve_spaceAndMovementTest2(file_folder):
     plt.show
 
 def retrieve_spaceAndMovementTest2__t1(file_folder):
+    """
+    This function retrieves the results from this test from a csv file and
+    plots only one trial.
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "spaceAndMovementTest2.csv", index_col=0)
 
-    # Font for Plot
+    # An empty plot with one subplot is created and formatting attributes are
+    # defined.
     font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 16}
     fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
 
+    # The subplot is filled with the corresponding data.
     for column in df.columns[1:4]:
         df[column].plot(ax=axes, x=df.columns[0])
 
+    # The plot is labeled properly.
     axes.set_title("Space And Movement - Trial 1", fontsize = 18)
     axes.set_xlabel("Time", fontsize=14)
     axes.legend(["Active: 5 & 3", "Passive: 4 & 1", "Total Population"], loc='upper center', bbox_to_anchor=(0.5, -0.16),
@@ -212,7 +271,18 @@ def retrieve_spaceAndMovementTest2__t1(file_folder):
     plt.show
 
 def draw_spaceAndMovementTest2(file_folder, lower, upper, cycle):
+    """
+    This function retrieves the results from this test from a csv file and
+    draws a map of one trial (cycle argument) for the time-steps between
+    lower and upper (arguments)
+    """
+    # The dataframe is retrieved from a csv file.
     df = pd.read_csv(file_folder + "spaceAndMovementTest2.csv", index_col=0)
+
+    # Loops through lower-upper to draw only
+    # those time-steps. In each iteration an entry is selected and the food
+    # crates are drawn in green, active organisms in red and passive
+    # organisms in blue.
     for i in range(lower, upper):
         lst_crate = json.loads(df["food_crates__"+cycle].iloc[i])
         crate_x = [x for x, y, z in lst_crate]
